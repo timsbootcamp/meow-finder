ca276eb0ced00a8f853ccee402836b75722ec11e
Khomych1004 <khomych100488@gmail.com>
2024-02-02 00:08:36 +0000

    simplifying the function of getting a fact about a cat. binding to the update of a specific tag p, id - fact

--- assets/js/api-thecat.js
+++ assets/js/api-thecat.js
@@ -1,5 +1,5 @@
 //'api-thecat.js' holds all Javascript related to accessing 'theCatAPI'
-const div = document.querySelector('#');
+const p = document.querySelector('#fact');
 
 const apiKey_TheCatAPI = "live_lu84yq93RYo14uTkB0E6v8sUCAVLkUuGhJ42BQJ9o4gEr1gBOa6nYPsks0QBPsXn";
 const baseUrl_TheCatAPI = `https://api.thecatapi.com/v1/images/search?size=med&mime_types=jpg&format=json&has_breeds=true&order=RANDOM&page=0&limit=1`;
@@ -16,11 +16,11 @@ var requestOptions = {
   redirect: 'follow'
 };
 
-//function that inserts a fact about a cat into HTML
+//functions for fact
+
+//function that inserts a fact about a cat into HTML tag p
 function updateHTML_RandomCatFact(fact) {
-  let p = document.createElement('p');
   p.textContent = fact;
-  div.append(p);
 }
 
 //getting a fact about a cat using API
