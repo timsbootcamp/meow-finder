fd08567634d7fca304da197edf1e8bb30d2e20e6
adahollins <adrianna.hollins@hotmail.com>
2024-02-02 13:17:42 +0000

    Added catalogue to search results with 'hide' feature

--- assets/js/catalogue.js
+++ assets/js/catalogue.js
@@ -18,7 +18,7 @@ function updateCatalogue_HTML_Dynamically(data) {
     let len = Object.entries(data).length
 
     for (var i = 0; i <= len - 1; i++) {
-        $(`#cat-breed${i + 1}`).text(data[i].name);
+        $(`#cat-breed-title${i + 1}`).text(data[i].name);
         $(`#playfulness-trait${i + 1}`).text("Playfulness");
         $(`#child-friendly-trait${i + 1}`).text("Child-Friendly");
         $(`#family-friendly-trait${i + 1}`).text("Family-Friendly");
